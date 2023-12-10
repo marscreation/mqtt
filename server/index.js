c28b485f00764611e0852287eb92606412a07a1e
marsVoyager <59786662+marscreation@users.noreply.github.com>
2023-12-10 13:00:31 +0800

    fix: db link

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cors());
 
 mongoose
-  .connect("mongodb://localhost:27017/mqtt", {
+  .connect(CONNECTION, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
